6a50afdc18b020c5e6e5c4df45f885d7295c6bb5
vain-Liang <127164184+vain-Liang@users.noreply.github.com>
2025-05-06 18:12:46 +0800

    Update env.js
    Change goolge DoH to ali DoH

--- src/core/env.js
+++ src/core/env.js
@@ -122,7 +122,7 @@ const defaults = new Map(
     // secondary doh upstream
     CF_DNS_RESOLVER_URL_2: {
       type: "string",
-      default: "https://dns.google/dns-query",
+      default: "https://dns.alidns.com/dns-query",
     },
     // upstream recursive rethinkdns resolver running on Fly.io
     MAX_DNS_RESOLVER_URL: {
